a76a43750a20df260033d419db98ac4bab973449
Rodrigo Siqueira <154473805+RodrigooNS@users.noreply.github.com>
2024-08-25 00:18:51 +0000

    alterada frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Fiz um site pra te dizer que tô com saudade! { "<3" } </h1>
+    return <h1>Fiz um site pra te lembrar que tu é lindo! Saudade { "<3" } </h1>
 }
 
 export default Home
