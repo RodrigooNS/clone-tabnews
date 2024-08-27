638ad41a3525e295c66ffbdb30957fe843a23ee3
RodrigoNS <rodrigoons@gmail.com>
2024-08-27 14:16:22 -0300

    adicina scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Fiz um site pra te lembrar que tu é lindo! Saudade { "<3" } </h1>
+  return <h1>Fiz um site pra te lembrar que tu é lindo! Saudade {"<3"} </h1>;
 }
 
-export default Home
+export default Home;
