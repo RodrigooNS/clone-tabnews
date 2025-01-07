a27c328067e534cc852f1e45357af1a35cf6b084
RodrigoNS <rodrigoons@gmail.com>
2025-01-07 15:17:40 -0300

    partially updated endpoint `/status` and its tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const postgresVerResult = await database.query("SHOW server_version;");
+  const postgresVer = postgresVerResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  // const activeConnectionsResult = await database.query(`
+  //     SELECT COUNT(*) AS active_connections
+  //     FROM pg_stat_activity;
+  //   `);
+  // const activeConnections = activeConnectionsResult.rows[0].active_connections;
+
+  const activeConnectionsResult = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'postgres';",
+  );
+  const activeConnections = activeConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: parseFloat(postgresVer),
+        max_connections: parseInt(maxConnections),
+        active_connections: activeConnections,
+      },
+    },
+  });
 }
 
 export default status;
