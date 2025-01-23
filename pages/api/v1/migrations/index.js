b234457cd2c7692c77d389d562bfbc8581173be0
RodrigoNS <rodrigoons@gmail.com>
2025-01-23 10:49:51 -0300

    updates `/migrations` endpoint error handling

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,23 +4,31 @@ import database from "infra/database.js";
 
 export default async function status(request, response) {
   if (request.method === "GET" || request.method === "POST") {
-    const dbClient = await database.getNewClient();
-    const migrations = await migrationRunner({
-      dbClient: dbClient,
-      dryRun: request.method === "GET",
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
+    let dbClient;
+    try {
+      dbClient = await database.getNewClient();
+      const migrations = await migrationRunner({
+        dbClient: dbClient,
+        dryRun: request.method === "GET",
+        dir: join("infra", "migrations"),
+        direction: "up",
+        verbose: true,
+        migrationsTable: "pgmigrations",
+      });
 
-    await dbClient.end();
-
-    if (migrations.length > 0) {
-      return response.status(201).json(migrations);
+      if (migrations.length > 0) {
+        return response.status(201).json(migrations);
+      }
+      return response.status(200).json(migrations);
+    } catch (e) {
+      console.error(e);
+      throw e;
+    } finally {
+      await dbClient.end();
     }
-    return response.status(200).json(migrations);
   }
 
-  return response.status(405).end();
+  return response.status(405).json({
+    error: `Method ${request.method} not allowed`,
+  });
 }
