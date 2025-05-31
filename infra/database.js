c8c5f0340277142774e98b9c33ac9488d7c7c87b
RodrigoNS <rodrigoons@gmail.com>
2025-05-30 22:14:53 -0300

    handles db connection with ssl and self-signed certificate

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,27 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
+    ssl: getSSLValues(),
   });
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (e) {
+    console.error(e);
     throw e;
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === 'development' ? false : true;
 }
 
 export default {
