89905265fbadf0def886d61cb340d0db01233718
RodrigoNS <rodrigoons@gmail.com>
2025-05-31 15:16:45 -0300

    restores `infra/migrations` dir and maintains db connection error fix at `api/v1/migrations/index.js`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,18 +2,19 @@ import { runner as migrationRunner } from 'node-pg-migrate';
 import database from 'infra/database';
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dir: 'pages/api/v1/migrations/migrations',
-    direction: 'up',
-    dryRun: true,
-    verbose: true,
-    migrationsTable: 'pgmigrations',
-  };
-
+  let dbClient;
   try {
+    dbClient = await database.getNewClient();
+
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dir: 'infra/migrations',
+      direction: 'up',
+      dryRun: true,
+      verbose: true,
+      migrationsTable: 'pgmigrations',
+    };
+
     if (request.method === 'GET') {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
       response.status(200).json(pendingMigrations);
