58eff548fe3e9e19a6afe1e206929f3928153e66
RodrigoNS <rodrigoons@gmail.com>
2025-05-31 15:00:11 -0300

    vercel/next build test

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -6,35 +6,39 @@ export default async function migrations(request, response) {
 
   const defaultMigrationOptions = {
     dbClient: dbClient,
-    dir: 'src/migrations',
+    dir: 'pages/api/v1/migrations/migrations',
     direction: 'up',
     dryRun: true,
     verbose: true,
     migrationsTable: 'pgmigrations',
   };
 
-  if (request.method === 'GET') {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    response.status(200).json(pendingMigrations);
-    await dbClient.end();
-    return;
-  }
-
-  if (request.method === 'POST') {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      response.status(201).json(migratedMigrations);
+  try {
+    if (request.method === 'GET') {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      response.status(200).json(pendingMigrations);
       return;
     }
-    response.status(200).json(migratedMigrations);
+
+    if (request.method === 'POST') {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0) {
+        response.status(201).json(migratedMigrations);
+        return;
+      }
+      response.status(200).json(migratedMigrations);
+      return;
+    }
+    response.status(405).end();
     return;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  } finally {
+    await dbClient.end();
   }
-  await dbClient.end();
-  response.status(405).end();
-  return;
 }
