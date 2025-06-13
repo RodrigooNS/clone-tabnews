ed8aaacf51d8a7d0503ac9df063e3dd815882132
RodrigoNS <rodrigoons@gmail.com>
2025-06-13 11:43:05 -0300

    test: change test description patterns

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,22 +6,30 @@ beforeAll(async () => {
   await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC");
 });
 
-test("POST to /api/v1/migrations should return 201", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response.status).toBe(201);
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("First try", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          { method: "POST" },
+        );
+        expect(response.status).toBe(201);
 
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-});
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
 
-test("Second POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+      test("Second try", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          { method: "POST" },
+        );
+        expect(response.status).toBe(200);
+        const responseBody = await response.json();
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
-  expect(responseBody.length).toBe(0);
 });
