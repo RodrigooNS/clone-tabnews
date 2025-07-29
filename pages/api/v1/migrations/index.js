a35853f2edf1d76bbec9850fbf5da96f6b3df5d5
RodrigoNS <rodrigoons@gmail.com>
2025-07-29 12:31:54 -0300

    refactor: use `next-connect` in `/migrations` and `/status`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,45 +1,56 @@
+import { createRouter } from "next-connect";
 import { runner as migrationRunner } from "node-pg-migrate";
 import { resolve } from "node:path";
-import database from "infra/database";
+import database from "infra/database.js";
+import controller from "infra/controller.js";
 
-export default async function migrations(request, response) {
+const defaultMigrationOptions = {
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  dryRun: true,
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+const router = createRouter();
+
+router.get(getHandler).post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
   let dbClient;
   try {
     dbClient = await database.getNewClient();
 
-    const defaultMigrationOptions = {
-      dbClient: dbClient,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      dryRun: true,
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
-
-    if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-      response.status(200).json(pendingMigrations);
-      return;
-    }
-
-    if (request.method === "POST") {
-      const migratedMigrations = await migrationRunner({
-        ...defaultMigrationOptions,
-        dryRun: false,
-      });
-
-      if (migratedMigrations.length > 0) {
-        response.status(201).json(migratedMigrations);
-        return;
-      }
-      response.status(200).json(migratedMigrations);
-      return;
-    }
-    response.status(405).end();
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+    response.status(200).json(pendingMigrations);
     return;
-  } catch (e) {
-    console.error(e);
-    throw e;
+  } finally {
+    await dbClient.end();
+  }
+}
+
+async function postHandler(request, response) {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+      dbClient,
+    });
+
+    if (migratedMigrations.length > 0) {
+      response.status(201).json(migratedMigrations);
+      return;
+    }
+    response.status(200).json(migratedMigrations);
+    return;
   } finally {
     await dbClient.end();
   }
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,32 +1,26 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import controller from "infra/controller.js";
 
-async function status(request, response) {
-  try {
-    const updatedAt = new Date().toISOString();
-    const pgVersion = await database.query("SHOW server_version;");
-    const pgMaxConnections = await database.query("SHOW max_connections;");
-    const pgActiveConnections = await database.query(
-      "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'dockerpostgres';",
-    );
-    response.status(200).json({
-      updated_at: updatedAt,
-      database: {
-        version: pgVersion.rows[0].server_version,
-        max_connections: parseInt(pgMaxConnections.rows[0].max_connections),
-        active_connections: pgActiveConnections.rows[0].count,
-      },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
+const router = createRouter();
 
-    console.log("\n Erro dentro do catch do controller:");
-    console.error(publicErrorObject);
+router.get(getHandler);
 
-    response.status(500).json(publicErrorObject);
-  }
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
+  const pgVersion = await database.query("SHOW server_version;");
+  const pgMaxConnections = await database.query("SHOW max_connections;");
+  const pgActiveConnections = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'dockerpostgres';",
+  );
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      version: pgVersion.rows[0].server_version,
+      max_connections: parseInt(pgMaxConnections.rows[0].max_connections),
+      active_connections: pgActiveConnections.rows[0].count,
+    },
+  });
 }
-
-export default status;
