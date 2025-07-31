e14a7d4d9bef9601c436e4525c869e402f02c987
RodrigoNS <rodrigoons@gmail.com>
2025-07-31 09:11:26 -0300

    refactor: use `migrator` model in `/api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,16 +1,6 @@
 import { createRouter } from "next-connect";
-import { runner as migrationRunner } from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
 import controller from "infra/controller.js";
-
-const defaultMigrationOptions = {
-  dir: resolve("infra", "migrations"),
-  direction: "up",
-  dryRun: true,
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
+import migrator from "models/migrator.js";
 
 const router = createRouter();
 
@@ -19,39 +9,16 @@ router.get(getHandler).post(postHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient,
-    });
-    response.status(200).json(pendingMigrations);
-    return;
-  } finally {
-    await dbClient.end();
-  }
+  const pendingMigrations = await migrator.listPendingMigrations();
+  response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-      dbClient,
-    });
-
-    if (migratedMigrations.length > 0) {
-      response.status(201).json(migratedMigrations);
-      return;
-    }
-    response.status(200).json(migratedMigrations);
+  const migratedMigrations = await migrator.runPendingMigrations();
+  if (migratedMigrations.length > 0) {
+    response.status(201).json(migratedMigrations);
     return;
-  } finally {
-    await dbClient.end();
   }
+  response.status(200).json(migratedMigrations);
+  return;
 }
