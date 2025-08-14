f0e31368be99caf70cedebb0dab49fe4fc54941b
RodrigoNS <rodrigoons@gmail.com>
2025-08-14 19:05:21 -0300

    feat: create and use `ValidationError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -55,3 +55,23 @@ export class ServiceError extends Error {
     };
   }
 }
+
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Um erro de validação ocorreu.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Verifique se os dados enviados estão corretos.";
+    this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
